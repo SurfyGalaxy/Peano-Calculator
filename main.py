de5373a95c3764b6ea8d0ce9f5c06d5a668e7787
SurfyGalaxy <ashton3141@gmail.com>
2026-08-24 17:41:39 +1000

    commital pt 2

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import tkinter as tk
 import tkinter.font as tkfont
 import json
 
+import playsound3 as ps
+
 root = tk.Tk()
 root.title("Peano Calculator")
-root.geometry("500x800")
+root.geometry("500x310")
 
 class Calculator:
     def __init__(self):
         self.mode = 2 # 0 = whole, 1 = int, 2 = rational
-        self.value = "126789ABCDEF"
-        self.display_font = tkfont.Font(family="TkFixedFont", size=50)
+        self.original = "               "
+        self.display = "               "
+        self.string = ""
+        self.display_font = tkfont.Font(family="DejaVu Sans Mono", size=40)
         for i in range(5):
             root.grid_columnconfigure(i, weight=1)
 
@@ -20,12 +24,40 @@ class Calculator:
             self.buttons = json.load(f)
 
     def main(self):
-        tk.Button(root, text=self.value, font=self.display_font).grid(column=0, row=0, columnspan=5, sticky="N")
+        tk.Button(root, text=self.display, font=self.display_font).grid(column=0, row=0, columnspan=5, sticky="N")
+        tk.Button(root, text="<-", command=lambda: self.inputs("<-")).grid(column=0, row=1, columnspan=2, sticky="NESW")
+        tk.Button(root, text=":3c", command=lambda:self.inputs(":3c")).grid(column=2, row=1, sticky="NESW")
+        tk.Button(root, text="->", command=lambda: self.inputs("->")).grid(column=3, row=1, columnspan=2, sticky="NESW")
         for name in self.buttons:
             data = self.buttons[name]
-            
-            button = tk.Button(root, text=data["text"], command=lambda: add(data["text"]))
-            button.grid(column=data["col"], row=data["row"], sticky="NESW")
+
+            button = tk.Button(
+                root,
+                text=data["text"],
+                command=lambda text=data["text"]: self.inputs(text)
+            )
+
+            button.grid(
+                column=data["col"],
+                row=data["row"],
+                sticky="NESW"
+            )
+
+            globals()[name] = button
+        
+    def inputs(self, value):
+        if value in {"<-", "->", "AC", "C", "M+", "M-", "MR", "MS", ":3c"}:
+            if value == ":3c":
+                play_cats()
+        
+        else:
+            self.string = self.string + value
+            if len(self.string) - len(self.original) < 0:
+                self.display = self.string + self.original[len(self.string):]
+            else:
+                self.display = "<" + self.string[-len(self.original):]
+        print(self.string)
+        self.main()
 
 calc = Calculator()
 calc.main()
