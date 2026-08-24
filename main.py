e2a940b13124b5069fb270f47060742fbfe32fce
SurfyGalaxy <ashton3141@gmail.com>
2026-08-24 16:13:59 +1000

    add buttons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logic
 import helper
 import tkinter as tk
 import tkinter.font as tkfont
+import json
 
 root = tk.Tk()
 root.title("Peano Calculator")
@@ -14,9 +15,17 @@ class Calculator:
         self.display_font = tkfont.Font(family="TkFixedFont", size=50)
         for i in range(5):
             root.grid_columnconfigure(i, weight=1)
-    
+
+        with open("buttons.json") as f:
+            self.buttons = json.load(f)
+
     def main(self):
-        tk.Label(root, text=self.value, font=self.display_font).grid(column=0, row=0, columnspan=5, sticky="N")
+        tk.Button(root, text=self.value, font=self.display_font).grid(column=0, row=0, columnspan=5, sticky="N")
+        for name in self.buttons:
+            data = self.buttons[name]
+            
+            button = tk.Button(root, text=data["text"], command=lambda: add(data["text"]))
+            button.grid(column=data["col"], row=data["row"], sticky="NESW")
 
 calc = Calculator()
 calc.main()
